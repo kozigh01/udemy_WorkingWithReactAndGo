370b9fa4a7f11ca06b2ba9eeee07a0994e38b524
Mark Kozisek <mark.kozisek@mastery.net>
2021-09-05 21:09:44 +0000

    Use 3rd party router and add first route

--- backend-app/cmd/api/main.go
+++ backend-app/cmd/api/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 const (
-	version = "1.0.0"
+	version = "1.1.0"
 )
 
 type config struct {
@@ -20,11 +20,11 @@ type config struct {
 
 type AppStatus struct {
 	Status      string `json:"status"`
-	Environment string `json:"environment"`
-	Version     string `json:"version"`
+	Environment string `json:"env"`
+	Version     string `json:"ver"`
 }
 
-type Application struct {
+type application struct {
 	config config
 	logger *log.Logger
 }
@@ -32,36 +32,27 @@ type Application struct {
 func main() {
 	var cfg config
 
-	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.IntVar(&cfg.port, "port", 4050, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	app := Application {
+
+	app := &application {
 		config: cfg,
 		logger: logger,
 	}
 
-	fmt.Printf("Running: port=%v, env=%q\n", cfg.port, cfg.env)
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d", cfg.port),
+		Handler: app.routes(),
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
 
-	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		currentStatus := AppStatus{
-			Status:      "Available",
-			Environment: cfg.env,
-			Version:     version,
-		}
-
-		js, err := json.MarshalIndent(currentStatus, "", "\t")
-		if err != nil {
-			log.Panicln(err)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(js)
-	})
-
-	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+	logger.Println("Starting server on port", cfg.port)
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panicln(err)
 	}
--- backend-app/cmd/api/statusHandler.go
+++ backend-app/cmd/api/statusHandler.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func(app *application) statusHandler(w http.ResponseWriter, r *http.Request) {
+	currentStatus := AppStatus{
+		Status:      "Available",
+		Environment: app.config.env,
+		Version:     version,
+	}
+
+	js, err := json.MarshalIndent(currentStatus, "", "\t")
+	// js, err := json.Marshal(currentStatus)
+	if err != nil {
+		app.logger.Panicln(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(append(js, []byte("\n")...))	
+}
